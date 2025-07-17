d7a8209309fb907ca5722d3a5d20e0ff618f1294
weni121 <weni1212312121@gmail.com>
2025-07-17 15:51:26 +0700

    Add files via upload

---
+++ ladsql01.sql
@@ -0,0 +1,13 @@
+---1---
+select * from Employees
+--2---
+select EmployeeID, FirstName, LastName from Employees
+---3---
+select * from Employees WHERE City = 'london'
+---4---
+select EmployeeID, FirstName, LastName from Employees
+select * from Employees WHERE City = 'london'
+---5---
+Select city, country from Customers
+---6---
+Select Distinct City, Country from Customers
