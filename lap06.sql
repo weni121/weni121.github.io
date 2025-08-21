036c234aa12764cd29c1f1bdd4776209b418ebf5
weni121 <weni1212312121@gmail.com>
2025-08-21 13:54:18 +0700

    Add files via upload

--- lap06.sql
+++ lap06.sql
@@ -1,1 +1,19 @@
-SELECT CategoryName
+SELECT CategoryName, ProductName, UnitPrice
+FROM Products as p, Categories as c
+WHERE p.CategoryID = c.CategoryID
+and CategoryName = 'seafood'
+
+SELECT CategoryName, ProductName, UnitPrice
+FROM Products Join Categories
+ON Products.CategoryID=Categories.CategoryID
+WHERE CategoryName = 'seafood'
+
+SELECT CompanyName, OrderID
+FROM Orders, ShipperID = Orders.Shipvia
+WHERE Shippers.ShipperID = Order.Shipvia
+AND OrderID = 10275
+
+SELECT CompanyName, OrderID
+FROM Orders JOIN Shippers
+WHERE Shippers.ShipperID = Order.Shipvia
+WHERE OrderID = 10275
