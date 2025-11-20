2c477d2cba9fc7ba419920c392dbfc9fa884146c
weni121 <weni1212312121@gmail.com>
2025-11-20 09:44:49 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo"hello, World";
+        echo"<br>";
+        echo"<h2>My Name is WeNi.</h2>";
+    ?>
+</body>
+</html>
